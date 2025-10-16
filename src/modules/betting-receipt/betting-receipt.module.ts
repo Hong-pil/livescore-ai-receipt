988e96d47349537f7e7f265375b8c25672d61435
Hong-pil <hpk4982@gmail.com>
2025-10-16 12:13:21 +0900

    ai 영수증 추천 알고리즘 구현 중

--- src/modules/betting-receipt/betting-receipt.module.ts
+++ src/modules/betting-receipt/betting-receipt.module.ts
@@ -2,18 +2,31 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BettingReceiptController } from './controllers/betting-receipt.controller';
+import { RecommendationController } from './controllers/recommendation.controller';
 import { BettingReceiptService } from './services/betting-receipt.service';
+import { RecommendationService } from './services/recommendation.service';
 import { BettingReceipt, BettingReceiptSchema } from './schemas/betting-receipt.schema';
 
 @Module({
   imports: [
     // MongoDB의 betting_receipts 컬렉션 사용 준비
+    // Schema: MongoDB에 저장될 데이터 형식 정의
     MongooseModule.forFeature([
       { name: BettingReceipt.name, schema: BettingReceiptSchema }
     ]),
   ],
-  controllers: [BettingReceiptController],  // 요청 받는 곳
-  providers: [BettingReceiptService],       // 비즈니스 로직 처리
-  exports: [BettingReceiptService],
+  controllers: [
+    BettingReceiptController,
+    RecommendationController
+  ],  // 요청 받는 곳
+    
+  providers: [
+    BettingReceiptService,
+    RecommendationService
+  ],       // 비즈니스 로직 처리
+  exports: [
+    BettingReceiptService,
+    RecommendationService
+  ],
 })
 export class BettingReceiptModule {}
